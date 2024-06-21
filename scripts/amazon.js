6610bfbfcf5dfa58d9c60c4cf0942a7622496ec7
abdulrahmoncodes <abdurrahmanabusaad@gmail.com>
2024-06-21 12:47:52 +0100

    added settimeout on the cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -54,6 +54,7 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
 	button.addEventListener("click", () => {
 		// const productId = button.dataset.productId;
+		let addedMessageTimeoutId;
 		const { productId } = button.dataset;
 
 		let matchingItem;
@@ -85,5 +86,20 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
 		});
 
 		document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+		const addedMessage = document.querySelector(
+			`.js-added-to-cart-${productId}`
+		);
+		addedMessage.classList.add("added-to-cart-visible");
+
+		if (addedMessageTimeoutId) {
+			clearTimeout(addedMessageTimeoutId);
+		}
+
+		const timeoutId = setTimeout(() => {
+			addedMessage.classList.remove("added-to-cart-visible");
+		}, 2000);
+
+		addedMessageTimeoutId = timeoutId;
 	});
 });
